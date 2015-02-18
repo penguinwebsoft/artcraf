0730b86d8d3146fa16b945eef06509da9e92caf0
prabaharan periasamy <prabaharanit@gmail.com>
2015-02-18 11:31:19 +0530

    manage seller service and test added

---
+++ crafartservice/src/main/java/com/crafart/service/ManageSellerService.java
@@ -0,0 +1,13 @@
+package com.crafart.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class ManageSellerService 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ crafartservice/src/test/java/com/crafart/ManageSellerTest.java
@@ -0,0 +1,5 @@
+package com.crafart;
+
+public class ManageSellerTest {
+
+}
