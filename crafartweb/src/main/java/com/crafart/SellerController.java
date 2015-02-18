1a7898648bb155993ec562f50d919a6a1fd56ff2
prabaharan periasamy <prabaharanit@gmail.com>
2015-02-18 11:32:44 +0530

    Initial version - seller controller and test classes

---
+++ crafartweb/src/main/java/com/crafart/SellerController.java
@@ -0,0 +1,13 @@
+package com.crafart;
+
+/**
+ * Hello world!
+ *
+ */
+public class SellerController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
