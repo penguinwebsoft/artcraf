e1471c04c12e1228cfbb00777135b5dac99006cd
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-02-23 16:03:55 +0530

    SellerDAO java is updated

--- crafartdataaccess/src/test/java/com/crafart/SellerDAOTest.java
+++ crafartdataaccess/src/test/java/com/crafart/SellerDAOTest.java
@@ -1,9 +1,69 @@
 package com.crafart;
 
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.data.SellerDAO;
+import com.crafart.dataobjects.SellerDO;
+import com.crafart.exception.UserDataException;
 
 /**
  * Unit test for simple App.
  */
-public class SellerDAOTest{
+@ContextConfiguration("classpath:crafartdatasource-context-test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class SellerDAOTest {
 
+	private static final Logger log = Logger.getLogger(SellerDAOTest.class);
+
+	@Autowired
+	private SellerDAO sellerDAOImpl;
+
+	@Test
+	@Rollback(true)
+	public void testAddSeller() {
+		try {
+			SellerDO sellerDO = getSellerDO();
+			log.info("Seller id is" + sellerDO.getSellerId());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	private SellerDO getSellerDO() {
+		SellerDO sellerDO = new SellerDO();
+		sellerDO.setFirstName("xxxx");
+		sellerDO.setLastName("yyyy");
+		sellerDO.setTin_no(1);
+		sellerDO.setCompanyName("penguin");
+		sellerDO.setCompanyLogo("pen");
+		sellerDO.setEpch_no("123");
+		sellerDO.setVat_no("123456a");
+		sellerDO.setCst_no("000");
+		sellerDO.setCommission("aaaa");
+		sellerDO.setStatus(1);
+		sellerDO.setApproved(1);
+		try {
+			sellerDAOImpl.addSeller(sellerDO);
+			sellerDO.setSellerId(sellerDO.getSellerId());
+		} catch (UserDataException uExp) {
+			uExp.printStackTrace();
+		}
+		return sellerDO;
+
+	}
 }
