602e8f35f48a149f8f6f3eb68ba254d0de3b7b31
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-02-23 16:06:04 +0530

    ManageSellerServiceImpl java file is added

---
+++ crafartservice/src/main/java/com/crafart/service/ManageSellerServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.crafart.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.data.SellerDAO;
+import com.crafart.dataobjects.SellerDO;
+import com.crafart.exception.UserDataException;
+import com.crafart.service.ManageSellerService;
+import com.crafart.service.businessobjects.SellerBO;
+import com.crafart.service.exception.UserServiceException;
+import com.crafart.service.mapper.BeanMapper;
+
+/**
+ * @author Karthi
+ * @version 1.0
+ * 
+ */
+@Service("manageSellerServiceImpl")
+public class ManageSellerServiceImpl implements ManageSellerService {
+
+	private static final Logger log = Logger
+			.getLogger(ManageSellerServiceImpl.class);
+
+	@Autowired
+	private BeanMapper beanMapper;
+
+	@Autowired
+	private SellerDAO sellerDAOImpl;
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void addSeller(SellerBO sellerBO) throws UserServiceException {
+
+		try {
+			SellerDO sellerDO = beanMapper.mapSellerBOToDO(sellerBO,
+					new SellerDO());
+			sellerDAOImpl.addSeller(sellerDO);
+			sellerBO.setSellerId(sellerDO.getSellerId());
+		} catch (UserDataException uExp) {
+			throw new UserServiceException("add new seller failed", uExp);
+		}
+	}
+
+}
