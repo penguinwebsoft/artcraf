fe687a985f8e73c6cbee828702c50c859985f7ab
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-02-23 16:06:30 +0530

    SellerBO java file added

---
+++ crafartservice/src/main/java/com/crafart/service/businessobjects/SellerBO.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package com.crafart.service.businessobjects;
+
+import java.io.Serializable;
+
+/**
+ * @author Karthi
+ * @version 1.0
+ * 
+ */
+public class SellerBO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3856310385699735633L;
+
+	private long sellerId;
+
+	private String firstName;
+
+	private String lastName;
+
+	private int tin_no;
+
+	private String companyName;
+
+	private String companyLogo;
+
+	private String epch_no;
+
+	private String vat_no;
+
+	private String cst_no;
+
+	private String commission;
+
+	private int status;
+
+	private int approved;
+
+	public long getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(long sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getTin_no() {
+		return tin_no;
+	}
+
+	public void setTin_no(int tin_no) {
+		this.tin_no = tin_no;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCompanyLogo() {
+		return companyLogo;
+	}
+
+	public void setCompanyLogo(String companyLogo) {
+		this.companyLogo = companyLogo;
+	}
+
+	public String getEpch_no() {
+		return epch_no;
+	}
+
+	public void setEpch_no(String epch_no) {
+		this.epch_no = epch_no;
+	}
+
+	public String getVat_no() {
+		return vat_no;
+	}
+
+	public void setVat_no(String vat_no) {
+		this.vat_no = vat_no;
+	}
+
+	public String getCst_no() {
+		return cst_no;
+	}
+
+	public void setCst_no(String cst_no) {
+		this.cst_no = cst_no;
+	}
+
+	public String getCommission() {
+		return commission;
+	}
+
+	public void setCommission(String commission) {
+		this.commission = commission;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getApproved() {
+		return approved;
+	}
+
+	public void setApproved(int approved) {
+		this.approved = approved;
+	}
+
+	
+
+	
+
+}
