6011c0365c3d1721b46f4efb241b19a8e5f3d419
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-02-25 11:28:43 +0530

    UserDataException changed to CrafartDataException

--- crafartdataaccess/src/main/java/com/crafart/data/SellerDAO.java
+++ crafartdataaccess/src/main/java/com/crafart/data/SellerDAO.java
@@ -3,9 +3,9 @@ package com.crafart.data;
 import org.springframework.stereotype.Repository;
 
 import com.crafart.dataobjects.SellerDO;
-import com.crafart.exception.UserDataException;
+import com.crafart.exception.CrafartDataException;
 @Repository("sellerDAO")
 public interface SellerDAO {
 
-	public void addSeller(SellerDO sellerDO) throws UserDataException;
+	public void addSeller(SellerDO sellerDO) throws CrafartDataException;
 }
