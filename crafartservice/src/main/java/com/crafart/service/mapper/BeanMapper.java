ef371b34fa8e4290e74d9c58698545d94b97adb0
prabaharan periasamy <prabaharanit@gmail.com>
2015-03-05 23:01:01 +0530

    conflict - taken repo code

--- crafartservice/src/main/java/com/crafart/service/mapper/BeanMapper.java
+++ crafartservice/src/main/java/com/crafart/service/mapper/BeanMapper.java
@@ -14,7 +14,7 @@ import com.crafart.service.businessobjects.SellerBO;
  * @version 1.0
  * 
  */
-@Component("beanMapper") 
+@Component("beanMapper")
 public class BeanMapper {
 
 	private static final Logger log = Logger.getLogger(BeanMapper.class);
@@ -24,14 +24,14 @@ public class BeanMapper {
 		sellerDO.setCommission(sellerBO.getCommission());
 		sellerDO.setCompanyLogo(sellerBO.getCompanyLogo());
 		sellerDO.setCompanyName(sellerBO.getCompanyName());
-		sellerDO.setCst_no(sellerBO.getCst_no());
-		sellerDO.setEpch_no(sellerBO.getEpch_no());
+		sellerDO.setCst_no(sellerBO.getCstNo());
+		sellerDO.setEpch_no(sellerBO.getEpchNo());
 		sellerDO.setFirstName(sellerBO.getFirstName());
 		sellerDO.setLastName(sellerBO.getLastName());
 		sellerDO.setSellerId(sellerBO.getSellerId());
 		sellerDO.setStatus(sellerBO.getStatus());
 		sellerDO.setTin_no(sellerDO.getStatus());
-		sellerDO.setVat_no(sellerBO.getVat_no());
+		sellerDO.setVat_no(sellerBO.getVatNo());
 		sellerDO.setGender(sellerBO.getGender());
 		sellerDO.setDateOfBirth(sellerBO.getDateOfBirth());
 		log.info("bean mapping for sellerBO to sellerDO");
@@ -44,14 +44,14 @@ public class BeanMapper {
 		sellerBO.setCommission(sellerDO.getCommission());
 		sellerBO.setCompanyLogo(sellerDO.getCompanyLogo());
 		sellerBO.setCompanyName(sellerDO.getCompanyName());
-		sellerBO.setCst_no(sellerDO.getCst_no());
-		sellerBO.setEpch_no(sellerDO.getEpch_no());
+		sellerBO.setCstNo(sellerDO.getCst_no());
+		sellerBO.setEpchNo(sellerDO.getEpch_no());
 		sellerBO.setFirstName(sellerDO.getFirstName());
 		sellerBO.setLastName(sellerDO.getLastName());
 		sellerBO.setSellerId(sellerDO.getSellerId());
 		sellerBO.setStatus(sellerDO.getStatus());
-		sellerBO.setTin_no(sellerDO.getTin_no());
-		sellerBO.setVat_no(sellerDO.getVat_no());
+		sellerBO.setTinNo(sellerDO.getTin_no());
+		sellerBO.setVatNo(sellerDO.getVat_no());
 		sellerBO.setGender(sellerDO.getGender());
 		sellerBO.setDateOfBirth(sellerDO.getDateOfBirth());
 		log.info("bean mapping for sellerDO to sellerBO");
@@ -59,4 +59,4 @@ public class BeanMapper {
 
 	}
 
-	}
+}
