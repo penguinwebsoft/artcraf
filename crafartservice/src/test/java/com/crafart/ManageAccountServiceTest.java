493195950cc0ac401fdd1f30b82c77199b5136ff
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-10 14:32:40 +0530

    ManageAccountServiceTest file added

---
+++ crafartservice/src/test/java/com/crafart/ManageAccountServiceTest.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.crafart;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.service.ManageAccountService;
+import com.crafart.service.businessobjects.AccountBO;
+import com.crafart.service.exception.CrafartServiceException;
+
+/**
+ * @author Karthi
+ * @version 1.0
+ * 
+ */
+@ContextConfiguration({ "classpath:crafartdatasource-context-test.xml", "classpath:crafartservice-context-test.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class ManageAccountServiceTest {
+
+	public static final Logger log = Logger.getLogger(ManageAccountServiceTest.class);
+
+	@Autowired
+	private ManageAccountService manageAccountServiceImpl;
+
+	@Test
+	@Rollback(false)
+	public void testAddBankAccountDetail() {
+		AccountBO accountBO = getAccountDaetail();
+		try {
+			manageAccountServiceImpl.addBankAccountDetail(accountBO);
+		} catch (CrafartServiceException crafartServiceException) {
+			crafartServiceException.printStackTrace();
+			Assert.fail();
+		}
+	}
+
+	private AccountBO getAccountDaetail() {
+		AccountBO accountBO = new AccountBO();
+		accountBO.setAccountName("wewe");
+		accountBO.setAccountNumber("000011100");
+		accountBO.setBankName("xxx");
+		accountBO.setBranchName("yyy");
+		accountBO.setIfscCode("101010aaa0");
+		accountBO.setMicrCode("111000aaa111");
+		accountBO.setType("qq");
+		return accountBO;
+	}
+}
