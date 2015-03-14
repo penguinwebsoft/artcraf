d97f734f2285921f8321537d4aea5e961d12b6ad
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-14 15:06:42 +0530

    session added in SellerController.java

--- crafartweb/src/main/java/com/crafart/SellerController.java
+++ crafartweb/src/main/java/com/crafart/SellerController.java
@@ -1,6 +1,7 @@
 package com.crafart;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.crafart.service.ManageSellerService;
+import com.crafart.service.businessobjects.AddressBO;
 import com.crafart.service.businessobjects.SellerBO;
+import com.crafart.service.businessobjects.StoreBO;
 import com.crafart.service.exception.CrafartServiceException;
 
 /**
@@ -23,6 +27,7 @@ import com.crafart.service.exception.CrafartServiceException;
  */
 @Controller("sellerController")
 @RequestMapping("seller")
+@SessionAttributes("sellerprofile")
 public class SellerController {
 
 	private static final Logger log = Logger.getLogger(SellerController.class);
@@ -43,11 +48,12 @@ public class SellerController {
 	 */
 	@RequestMapping(value = { "/addSeller" }, method = RequestMethod.POST)
 	public @ResponseBody
-	ModelMap addSeller(@RequestBody SellerBO sellerBO, HttpServletRequest httpServletRequest) {
+	ModelMap addSeller(@RequestBody SellerBO sellerBO, StoreBO storeBO, AddressBO addressBO, HttpServletRequest httpServletRequest, HttpSession session) {
 		ModelMap modelMap = new ModelMap();
 		try {
 			log.info("controller");
 			manageSellerServiceImpl.addSeller(sellerBO);
+			session.setAttribute("sellerprofile", sellerBO);
 			log.info("added successfully");
 		} catch (Exception uExp) {
 			log.error("Error while adding seller", uExp);
@@ -55,4 +61,5 @@ public class SellerController {
 		return modelMap;
 
 	}
+
 }
