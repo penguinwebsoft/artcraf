b198c9f0386dc52899ee9fe18527eaa2bd7fa7c3
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 18:56:28 +0530

    transactional annotation added in AddressDAOTest

--- crafartdataaccess/src/test/java/com/crafart/AddressDAOTest.java
+++ crafartdataaccess/src/test/java/com/crafart/AddressDAOTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.crafart.data.AddressDAO;
@@ -29,7 +30,7 @@ import com.crafart.exception.CrafartDataException;
 public class AddressDAOTest {
 
 	@SuppressWarnings("unused")
-	private static final Logger log = Logger.getLogger(SellerDAOTest.class);
+	private static final Logger log = Logger.getLogger(AddressDAOTest.class);
 
 	@Autowired
 	private AddressDAO addressDAOImpl;
@@ -46,8 +47,8 @@ public class AddressDAOTest {
 		}
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED)
 	private AddressDO getAddress() {
-
 		AddressDO addressDO = new AddressDO();
 		addressDO.setCityId(12);
 		addressDO.setPinCode("000000");
