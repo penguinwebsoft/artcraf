177d8118af2d768979d89d4d26d804ff1d3fd53c
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 20:22:51 +0530

    removed unwanted printstacktrace() in GeoZoneController.java

--- crafartweb/src/main/java/com/crafart/GeoZoneController.java
+++ crafartweb/src/main/java/com/crafart/GeoZoneController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.crafart.exception.CrafartDataException;
 import com.crafart.service.ManageGeoZoneService;
 import com.crafart.service.businessobjects.GeoZoneBO;
+import com.crafart.service.exception.CrafartServiceException;
 
 /**
  * @author Karthi
@@ -26,6 +27,8 @@ import com.crafart.service.businessobjects.GeoZoneBO;
 @Controller("geoZoneController")
 @RequestMapping("geoZone")
 public class GeoZoneController {
+
+	private static final Logger log = Logger.getLogger(GeoZoneController.class);
 
 	@Autowired
 	ManageGeoZoneService manageGeoZoneServiceImpl;
@@ -38,8 +41,8 @@ public class GeoZoneController {
 		try {
 			geoZoneBOs = manageGeoZoneServiceImpl.getGeoZoneDetail();
 			modelMap.addAttribute("geoZoneBOs", geoZoneBOs);
-		} catch (CrafartDataException e) {
-			e.printStackTrace();
+		} catch (CrafartServiceException crafartServiceException) {
+			log.error("Application-error in retrieving details from DB", crafartServiceException);
 		}
 		return modelMap;
 
