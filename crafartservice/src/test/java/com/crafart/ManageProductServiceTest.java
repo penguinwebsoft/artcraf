e2a9a67b269f01a211c6c5b49c6f7814a3c8a517
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 19:17:57 +0530

    productDescription is added in ManageProductServiceTest.java

--- crafartservice/src/test/java/com/crafart/ManageProductServiceTest.java
+++ crafartservice/src/test/java/com/crafart/ManageProductServiceTest.java
@@ -4,7 +4,6 @@
 package com.crafart;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.crafart.service.ManageProductService;
 import com.crafart.service.ManageSellerService;
+import com.crafart.service.businessobjects.AddressBO;
+import com.crafart.service.businessobjects.LengthClassBO;
 import com.crafart.service.businessobjects.ProductBO;
+import com.crafart.service.businessobjects.ProductDescriptionBO;
 import com.crafart.service.businessobjects.SellerBO;
+import com.crafart.service.businessobjects.StoreBO;
+import com.crafart.service.businessobjects.WeightClassBO;
 import com.crafart.service.exception.CrafartServiceException;
 
 /**
  * @author Karthi
  * @version 1.0
- *
+ * 
  */
 @ContextConfiguration({ "classpath:crafartdatasource-context-test.xml", "classpath:crafartservice-context-test.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,15 +40,14 @@ public class ManageProductServiceTest {
 
 	@Autowired
 	private ManageProductService manageProductServiceImpl;
-	
+
 	@Autowired
 	private ManageSellerService manageSellerServiceImpl;
-	
+
 	@Test
-	@Ignore
 	@Rollback(true)
-	public void testManageProductService(){
-		
+	public void testManageProductService() {
+
 		ProductBO productBO = getProductBO();
 		try {
 			manageProductServiceImpl.addProduct(productBO);
@@ -53,20 +56,31 @@ public class ManageProductServiceTest {
 			Assert.fail();
 		}
 	}
-	
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	private ProductDescriptionBO getProductDescriptionAndSeo() {
+		ProductDescriptionBO productDescriptionBO = new ProductDescriptionBO();
+		productDescriptionBO.setDescription("qwerty");
+		productDescriptionBO.setMetaDescription("qwerty");
+		productDescriptionBO.setMetaKeyword("qwerty");
+		productDescriptionBO.setName("ytrewq");
+		productDescriptionBO.setTag("ytrewq");
+		return productDescriptionBO;
+	}
+
 	@Transactional(propagation = Propagation.REQUIRED)
 	private ProductBO getProductBO() {
 		SellerBO sellerBO = getSellerBO();
 		ProductBO productBO = new ProductBO();
 		productBO.setCategoryId(1);
-		productBO.setDateAvailable(java.sql.Date.valueOf("2013-02-01"));
+		productBO.setDateAvailable("20/00/0000");
 		productBO.setHeight(52);
 		productBO.setImage("a15cb5e");
 		productBO.setLength(63.2);
-		productBO.setLengthClassId(1);
-		productBO.setLocation("awawaw");
+		productBO.setLengthClassBO(getLengthClassBO(productBO));
+		productBO.setLocation("from service testing");
 		productBO.setMinimum(26.00);
-		productBO.setModel("zzzz");
+		productBO.setModel("service");
 		productBO.setPoints(5);
 		productBO.setPrice(12.2f);
 		productBO.setQuantity("2");
@@ -81,36 +95,78 @@ public class ManageProductServiceTest {
 		productBO.setUpc("asd");
 		productBO.setViewed(2);
 		productBO.setWeight("25");
-		productBO.setWeightClassId(1);
+		productBO.setWeightClassBO(getWeightClassBO(productBO));
 		productBO.setWidth(12.5);
+		productBO.setProductDescriptionBO(getProductDescriptionAndSeo());
 		return productBO;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED)
+	private WeightClassBO getWeightClassBO(ProductBO productBO) {
+		WeightClassBO weightClassBO = new WeightClassBO();
+		weightClassBO.setTitle("from service");
+		// weightClassBO.setUnit(15);
+		weightClassBO.setValue(10);
+		return weightClassBO;
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	private LengthClassBO getLengthClassBO(ProductBO productBO) {
+		LengthClassBO lengthClassBO = new LengthClassBO();
+		lengthClassBO.setTitle("from service");
+		lengthClassBO.setUnit(152);
+		lengthClassBO.setValue(Float.valueOf("15.2"));
+		return lengthClassBO;
+	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	private SellerBO getSellerBO() {
 		SellerBO sellerBO = new SellerBO();
-		sellerBO.setFirstName("xxxx");
-		sellerBO.setLastName("yyyy");
+		sellerBO.setDateOfBirth("00/00/0000");
 		sellerBO.setGender(1);
 		sellerBO.setDateOfBirth("00/00/0000");
-		sellerBO.setTinNo(1);
-		sellerBO.setCompanyName("penguin");
-		sellerBO.setCompanyLogo("pen");
-		sellerBO.setEpchNo("123");
-		sellerBO.setVatNo("123456a");
-		sellerBO.setCstNo("000");
-		sellerBO.setCommission("aaaa");
-		sellerBO.setStatus(1);
 		sellerBO.setApproved(1);
-		
-			try {
-				manageSellerServiceImpl.addSeller(sellerBO);
-			} catch (CrafartServiceException e) {
-				e.printStackTrace();
-			}
-			return sellerBO;
-			
+		sellerBO.setCommission("www");
+		sellerBO.setCompanyLogo("qqq");
+		sellerBO.setCompanyName("penguin");
+		sellerBO.setCstNo("4444");
+		sellerBO.setEpchNo("121212");
+		sellerBO.setFirstName("from service");
+		sellerBO.setLastName("service");
+		sellerBO.setStatus(2);
+		sellerBO.setTinNo(2);
+		sellerBO.setVatNo("asd123");
+		sellerBO.setStoreBO(getStoreBO(sellerBO));
+		sellerBO.setAddressBO(getAddressBO(sellerBO));
+
+		try {
+			manageSellerServiceImpl.addSeller(sellerBO);
+		} catch (CrafartServiceException e) {
+			e.printStackTrace();
+		}
+		return sellerBO;
+
+	}
+
+	private AddressBO getAddressBO(SellerBO sellerBO) {
+		AddressBO addressBO = new AddressBO();
+		addressBO.setCityId(0);
+		addressBO.setPinCode("service now");
+		addressBO.setStateId(0);
+		addressBO.setStreet("testing now");
+		addressBO.setSellerBO(sellerBO);
+		return addressBO;
+	}
+
+	private StoreBO getStoreBO(SellerBO sellerBO) {
+		StoreBO storeBO = new StoreBO();
+		storeBO.setName("from service add");
+		storeBO.setSellerBO(sellerBO);
+		storeBO.setStoreDescription(" from service add");
+		storeBO.setStoreReturn("serv");
+		storeBO.setStoreUrl("www.wwww.com");
+		return storeBO;
+
 	}
 
 }
