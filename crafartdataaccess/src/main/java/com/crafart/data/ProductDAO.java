cb2de671e7d20ae36f92b97b47c10135b67666bc
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 18:46:39 +0530

    repository annotation is added in ProductDAO.java

--- crafartdataaccess/src/main/java/com/crafart/data/ProductDAO.java
+++ crafartdataaccess/src/main/java/com/crafart/data/ProductDAO.java
@@ -3,6 +3,8 @@
  */
 package com.crafart.data;
 
+import org.springframework.stereotype.Repository;
+
 import com.crafart.dataobjects.ProductDO;
 import com.crafart.exception.CrafartDataException;
 
@@ -10,6 +12,7 @@ import com.crafart.exception.CrafartDataException;
  * @author Karthi
  * 
  */
+@Repository("productDAO")
 public interface ProductDAO {
 
 	public int addProduct(ProductDO productDO) throws CrafartDataException;
