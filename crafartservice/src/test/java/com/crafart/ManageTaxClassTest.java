f0832968d87cbb5dcafa65d6e19a1c15c8a46819
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 19:18:25 +0530

    ManageTaxClassTest.java file is added

---
+++ crafartservice/src/test/java/com/crafart/ManageTaxClassTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.crafart;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.service.ManageTaxClassService;
+import com.crafart.service.businessobjects.TaxClassBO;
+import com.crafart.service.exception.CrafartServiceException;
+
+/**
+ * @author karthi
+ * 
+ */
+@ContextConfiguration({ "classpath:crafartdatasource-context-test.xml", "classpath:crafartservice-context-test.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class ManageTaxClassTest {
+
+	@Autowired
+	private ManageTaxClassService manageTaxClassServiceImpl;
+
+	@Test
+	@Rollback(true)
+	public void testGetTaxClassDetail() {
+		try {
+			List<TaxClassBO> taxClassBOs = manageTaxClassServiceImpl.getTaxClassDetail();
+			for (TaxClassBO taxClassBO : taxClassBOs) {
+				System.out.print("\n" + taxClassBO.getTaxClassId());
+			}
+		} catch (CrafartServiceException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
