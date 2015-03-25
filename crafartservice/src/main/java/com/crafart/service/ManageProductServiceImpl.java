a69561143f298b6bf18ae91464a72794ec2d2fd9
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 19:08:02 +0530

    updated addProduct() with product description as a single unit

--- crafartservice/src/main/java/com/crafart/service/ManageProductServiceImpl.java
+++ crafartservice/src/main/java/com/crafart/service/ManageProductServiceImpl.java
@@ -9,10 +9,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.crafart.data.LengthClassDAO;
 import com.crafart.data.ProductDAO;
+import com.crafart.data.ProductDescriptionDAO;
+import com.crafart.data.WeightClassDAO;
+import com.crafart.dataobjects.LengthClassDO;
 import com.crafart.dataobjects.ProductDO;
+import com.crafart.dataobjects.ProductDescriptionDO;
+import com.crafart.dataobjects.WeightClassDO;
 import com.crafart.exception.CrafartDataException;
+import com.crafart.service.businessobjects.LengthClassBO;
 import com.crafart.service.businessobjects.ProductBO;
+import com.crafart.service.businessobjects.ProductDescriptionBO;
+import com.crafart.service.businessobjects.WeightClassBO;
 import com.crafart.service.exception.CrafartServiceException;
 import com.crafart.service.mapper.BeanMapper;
 
@@ -32,6 +41,15 @@ public class ManageProductServiceImpl implements ManageProductService {
 	@Autowired
 	private ProductDAO productDAOImpl;
 
+	@Autowired
+	private ProductDescriptionDAO productDescriptionDAOImpl;
+
+	@Autowired
+	private WeightClassDAO weightClassDAOImpl;
+
+	@Autowired
+	private LengthClassDAO lengthClassDAOImpl;
+
 	/**
 	 * mapping is done by mapProductBOToDO by this we are mapping BO objects to
 	 * DO then calling dataaccess addProduct method by productDO object
@@ -39,11 +57,23 @@ public class ManageProductServiceImpl implements ManageProductService {
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void addProduct(ProductBO productBO) throws CrafartServiceException {
-
-		ProductDO productDO = beanMapper.mapProductBOToDO(productBO, new ProductDO());
+		WeightClassBO weightClassBO = productBO.getWeightClassBO();
+		WeightClassDO weightClassDO = beanMapper.mapWeightClassBOToDO(weightClassBO, new WeightClassDO());
+		LengthClassBO lengthClassBO = productBO.getLengthClassBO();
+		LengthClassDO lengthClassDO = beanMapper.mapLengthClassBOToDO(lengthClassBO, new LengthClassDO());
+		ProductDO productDO = beanMapper.mapProductBOToDO(productBO, new ProductDO(), weightClassDO, lengthClassDO);
+		ProductDescriptionBO productDescriptionBO = productBO.getProductDescriptionBO();
+		ProductDescriptionDO productDescriptionDO = beanMapper.mapProductDescriptionBOToDO(productDescriptionBO, new ProductDescriptionDO(), productDO);
+		productDO.setWeightClassDO(weightClassDO);
+		productDO.setLengthClassDO(lengthClassDO);
 		try {
+			weightClassDAOImpl.addWeightClass(weightClassDO);
+			lengthClassDAOImpl.addLengthClass(lengthClassDO);
 			productDAOImpl.addProduct(productDO);
+			productDescriptionDAOImpl.addDescription(productDescriptionDO);
 			productBO.setProductId(productDO.getProductId());
+			productBO.getLengthClassBO().setLengthClassId(productDO.getLengthClassDO().getLengthClassId());
+			productBO.getWeightClassBO().setWeightClassId(productDO.getWeightClassDO().getWeightClassId());
 		} catch (CrafartDataException crafartDataException) {
 			log.error("Exception while adding ProductId " + productBO.getProductId());
 			crafartDataException.printStackTrace();
