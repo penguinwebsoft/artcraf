34992d934d6cb94e01bdb245cf85729ab5e85552
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-25 19:02:16 +0530

    TaxClassDAOTest file is added

---
+++ crafartdataaccess/src/test/java/com/crafart/TaxClassDAOTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.crafart;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.data.TaxClassDAO;
+import com.crafart.dataobjects.TaxClassDO;
+import com.crafart.exception.CrafartDataException;
+
+/**
+ * @author Karthi
+ * 
+ */
+@ContextConfiguration("classpath:crafartdatasource-context-test.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class TaxClassDAOTest {
+
+	@Autowired
+	public TaxClassDAO taxClassDAOImpl;
+
+	/*
+	 * @Test
+	 * 
+	 * @Rollback(true) public void testaddTaxClassDetail(){ TaxClassDO
+	 * taxClassDO = getTaxClass(); try {
+	 * taxClassDAOImpl.addTaxClassDetail(taxClassDO); } catch
+	 * (CrafartDataException e) { e.printStackTrace(); Assert.fail(); } }
+	 */
+
+	private TaxClassDO getTaxClass() {
+		TaxClassDO taxClassDO = new TaxClassDO();
+		taxClassDO.setDescription("VAT %");
+		taxClassDO.setTitle("VAT");
+		return taxClassDO;
+	}
+
+	@Test
+	@Rollback(true)
+	public void testGetTaxClassDetail() {
+		try {
+			List<TaxClassDO> taxClassDOs = taxClassDAOImpl.getTaxClassDetail();
+			for (TaxClassDO taxClassDO : taxClassDOs) {
+				System.out.print("\n" + taxClassDO.getTaxClassId());
+			}
+		} catch (CrafartDataException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
