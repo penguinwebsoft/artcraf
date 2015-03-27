05dbbc1038ede7fba49b53dc0ad2defa57f16543
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-03-27 21:34:48 +0530

    ManageCourierService and Impl file added

---
+++ crafartservice/src/main/java/com/crafart/service/ManageCourierService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.crafart.service;
+
+import java.util.List;
+
+import com.crafart.service.businessobjects.CourierBO;
+import com.crafart.service.exception.CrafartServiceException;
+
+/**
+ * @author Karthi
+ * 
+ */
+public interface ManageCourierService {
+
+	public void addCourierDetail(CourierBO courierBO) throws CrafartServiceException;
+
+	public List<CourierBO> getCourierDetail() throws CrafartServiceException;
+}
