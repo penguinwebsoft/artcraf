2f0a07daf24b892e6036714602c19476879648f4
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-04-02 21:04:32 +0530

    ProductAttributeDAOImpl.java file added

---
+++ crafartdataaccess/src/main/java/com/crafart/data/ProductAttributeDAOImpl.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.crafart.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.dataobjects.ProductAttributeDO;
+import com.crafart.exception.CrafartDataException;
+import com.crafart.inter.data.ProductAttributeDAO;
+
+/**
+ * @author Karthi
+ * 
+ */
+@Repository("productAttributeDAOImpl")
+public class ProductAttributeDAOImpl implements ProductAttributeDAO {
+
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void addProductAttribute(List<ProductAttributeDO> productAttributeDOs) throws CrafartDataException {
+		List<ProductAttributeDO> productAttributeDOs2 = productAttributeDOs;
+		try {
+			Session session = this.sessionFactory.openSession();
+			session.beginTransaction();
+			for (ProductAttributeDO productAttributeDO : productAttributeDOs2) {
+				session.persist(productAttributeDO);
+			}
+			session.getTransaction().commit();
+			session.close();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("DB error while adding product-attribute", hExp);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<ProductAttributeDO> getProductAttribute() throws CrafartDataException {
+		List<ProductAttributeDO> productAttributeDOs = new ArrayList<>();
+		try {
+			Session session = this.sessionFactory.openSession();
+			session.beginTransaction();
+			productAttributeDOs = session.createQuery("from ProductAttributeDO").list();
+			session.getTransaction().commit();
+			session.close();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("DB error while retriving data from product_attribute", hExp);
+		}
+		return productAttributeDOs;
+	}
+
+}
