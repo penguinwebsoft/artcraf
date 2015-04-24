f1d54ecb53eaebc4fe43e2d757549c28edeb7072
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-04-24 17:02:28 +0530

    New method login ,checkStoreUrl are added

--- crafartweb/src/main/java/com/crafart/SellerController.java
+++ crafartweb/src/main/java/com/crafart/SellerController.java
@@ -5,17 +5,21 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.crafart.inter.service.ManageSellerService;
+import com.crafart.inter.service.ManageStoreService;
 import com.crafart.service.businessobjects.AddressBO;
+import com.crafart.service.businessobjects.ContactBO;
 import com.crafart.service.businessobjects.SellerBO;
 import com.crafart.service.businessobjects.StoreBO;
 import com.crafart.service.exception.CrafartServiceException;
@@ -35,6 +39,12 @@ public class SellerController {
 	@Autowired
 	private ManageSellerService manageSellerServiceImpl;
 
+	@Autowired
+	private ManageStoreService manageStoreServiceImpl;
+
+	@Autowired
+	private PasswordEncoder bCryptPasswordEncoder;
+
 	/**
 	 * controller method responsible to add new seller details, it takes seller
 	 * details {@link SellerBO} from the user. Error during the adding process
@@ -51,10 +61,15 @@ public class SellerController {
 	ModelMap addSeller(@RequestBody SellerBO sellerBO, StoreBO storeBO, AddressBO addressBO, HttpServletRequest httpServletRequest, HttpSession session) {
 		ModelMap modelMap = new ModelMap();
 		try {
+			sellerBO.setPassword(bCryptPasswordEncoder.encode(sellerBO.getPassword()));
 			manageSellerServiceImpl.addSeller(sellerBO);
 			session.setAttribute("sellerprofile", sellerBO);
+			modelMap.addAttribute("result", true);
+			modelMap.addAttribute("message", "saved successfully");
 		} catch (CrafartServiceException crafartServiceException) {
 			log.error("Application-Error while adding seller", crafartServiceException);
+			modelMap.addAttribute("result", false);
+			modelMap.addAttribute("message", "error while adding");
 		}
 		return modelMap;
 
@@ -86,6 +101,51 @@ public class SellerController {
 			manageSellerServiceImpl.updateSeller(sellerBO2);
 		} catch (CrafartServiceException crafartServiceException) {
 			log.error("Application-error while updating sellertax", crafartServiceException);
+		}
+		return modelMap;
+
+	}
+
+	@RequestMapping(value = { "/checkStoreUrlAvailability" }, method = RequestMethod.POST)
+	public @ResponseBody
+	ModelMap checkStoreUrl(@RequestParam(value = "shopUrl") String storeUrl, HttpSession session) {
+		ModelMap modelMap = new ModelMap();
+		try {
+			StoreBO storeBO = manageStoreServiceImpl.checkStoreUrl(storeUrl);
+			if (storeBO != null) {
+				modelMap.addAttribute("return", true);
+				modelMap.addAttribute("message", "ShopURL exist try other");
+			} else {
+				modelMap.addAttribute("message", "ShopURL Available");
+			}
+		} catch (CrafartServiceException crafartServiceException) {
+			log.error("Application-error while checking shopurl", crafartServiceException);
+		}
+		return modelMap;
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public @ResponseBody
+	ModelMap findByEmailId(@RequestParam(value = "userEMailId") String email, @RequestParam(value = "userPassword") String password, HttpSession session) {
+		ModelMap modelMap = new ModelMap();
+		try {
+			if (email.isEmpty() || password.isEmpty()) {
+				log.info("not an user, login un-successful" + email + ", " + password);
+				modelMap.addAttribute("message", "Username or Password cant be empty.");
+				return modelMap;
+			}
+			ContactBO contactBO = manageSellerServiceImpl.findByEmailId(email);
+			if (contactBO != null && contactBO.getContactValue().equals(email)) {
+				if (bCryptPasswordEncoder.matches(password, contactBO.getSellerBO().getPassword()))
+					modelMap.addAttribute("message", "success");
+				else
+					modelMap.addAttribute("message", "password is mismatch");
+			} else {
+				modelMap.addAttribute("message", "User not exist");
+			}
+		} catch (CrafartServiceException crafartServiceException) {
+			log.error("Application error while checking normal login credentials", crafartServiceException);
 		}
 		return modelMap;
 
