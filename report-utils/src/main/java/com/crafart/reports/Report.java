e48b34d12d6dba131f365e43c6b7a306ea54d83b
prabaharan periasamy <prabaharanit@gmail.com>
2015-04-27 16:28:53 +0530

    code refactored - remvoed un used imports

--- report-utils/src/main/java/com/crafart/reports/Report.java
+++ report-utils/src/main/java/com/crafart/reports/Report.java
@@ -3,13 +3,12 @@ package com.crafart.reports;
 import org.springframework.stereotype.Service;
 
 import com.crafart.exception.ReportException;
-import com.crafart.model.OrderDeclarationBO;
 import com.crafart.model.ReportModel;
 
 @Service
 public interface Report {
 
-	public void generateReport(OrderDeclarationBO orderDeclarationBO)throws ReportException;
+	public void generateReport(ReportModel orderDeclarationBO)throws ReportException;
 	
-	public void generateDeclrationReport(ReportModel model) throws ReportException;
+	public void generateDeclarationReport(ReportModel model) throws ReportException;
 }
