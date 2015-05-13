e61d52b26f1be489f0e6e4999b45e881b1b14c79
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-05-13 21:38:47 +0530

    New version of ManageProductRatingService and ManageProductReviewService file are added

---
+++ crafartservice/src/main/java/com/crafart/inter/service/ManageProductReviewService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.crafart.inter.service;
+
+import java.util.List;
+
+import com.crafart.service.businessobjects.ProductReviewBO;
+import com.crafart.service.exception.CrafartServiceException;
+
+/**
+ * @author Karthi
+ * 
+ */
+public interface ManageProductReviewService {
+
+	public List<ProductReviewBO> getProductReview(long customerId) throws CrafartServiceException;
+}
