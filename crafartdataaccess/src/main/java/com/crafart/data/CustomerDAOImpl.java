c7306bde64b963ad81a64faa32d9f58ea92ae004
karthikeyan <karthikeyan.pugalur@gmail.com>
2015-05-13 21:24:12 +0530

    New methods added in CustomerDAOImpl

--- crafartdataaccess/src/main/java/com/crafart/data/CustomerDAOImpl.java
+++ crafartdataaccess/src/main/java/com/crafart/data/CustomerDAOImpl.java
@@ -4,6 +4,7 @@
 package com.crafart.data;
 
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +41,36 @@ public class CustomerDAOImpl implements CustomerDAO {
 			session.close();
 		} catch (HibernateException hExp) {
 			throw new CrafartDataException("DB error while adding customer detail", hExp);
-		}
+	}
 	}
 
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public CustomerDO getCustomerDetails(long customerId) throws CrafartDataException {
+		CustomerDO customerDO = new CustomerDO();
+		try {
+			Session session = this.sessionFactory.openSession();
+			session.beginTransaction();
+			Query query = session.createQuery("from CustomerDO where customer_id = :customer_id");
+			query.setLong("customer_id", customerId);
+			customerDO = (CustomerDO) query.uniqueResult();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("Error while retriving customer details", hExp);
+		}
+		return customerDO;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void updateCustomerDetails(CustomerDO customerDO) throws CrafartDataException {
+		try {
+			Session session = this.sessionFactory.openSession();
+			session.beginTransaction();
+			session.update(customerDO);
+			session.getTransaction().commit();
+			session.close();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("Erroe while updating customer details", hExp);
+		}
+	}
 }
