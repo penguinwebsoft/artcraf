de456c9609e41811c85a12e8e8acaa1d9a8bd497
prabaharan periasamy <prabaharanit@gmail.com>
2015-06-10 13:44:18 +0530

    bug fix - commission bo passed to seller instead of commission value

--- crafartweb/src/test/java/com/crafart/ProductControllerTest.java
+++ crafartweb/src/test/java/com/crafart/ProductControllerTest.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 
 import com.crafart.service.businessobjects.AddressBO;
+import com.crafart.service.businessobjects.CommissionBO;
 import com.crafart.service.businessobjects.ContactBO;
 import com.crafart.service.businessobjects.LengthClassBO;
 import com.crafart.service.businessobjects.ProductAttributeBO;
@@ -256,7 +257,7 @@ public class ProductControllerTest {
 		sellerBO.setGender(1);
 		sellerBO.setDateOfBirth("00/00/0000");
 		sellerBO.setApproved(1);
-		sellerBO.setCommission("www");
+		sellerBO.setCommissionBO(new CommissionBO());
 		sellerBO.setCompanyLogo("qqq");
 		sellerBO.setPassword("jsavdjhv");
 		sellerBO.setCompanyName("penguin");
