4995c6b96f4d729e54077ac095429dd7da5863e2
Rubiga <rubigakannan@gmail.com>
2015-06-22 17:27:43 +0530

    to change courier table name to courier

--- crafart-db-files/crafart/db/scripts/courier.sql
+++ crafart-db-files/crafart/db/scripts/courier.sql
@@ -2,8 +2,15 @@ CREATE TABLE COURIER (
 	COURIER_ID BIGINT PRIMARY KEY NOT NULL,
 	NAME VARCHAR(45),
 	IMAGE VARCHAR(45),
-	PRODUCT_LIMIT SMALLINT,
+    
 	SORT_ORDER SMALLINT,
 	CREATED_DTTM TIMESTAMP DEFAULT NOW(),
 	MODIFIED_DTTM TIMESTAMP DEFAULT NOW()
 	)
+	
+	
+	/************** alter table name to courier_name ***********/
+	ALTER TABLE courier ADD COLUMN courier_name character varying(45);
+    ALTER TABLE courier ADD COLUMN estimated_delivery_time time with time zone;
+    ALTER TABLE courier DROP COLUMN product_limit small int;
+   
