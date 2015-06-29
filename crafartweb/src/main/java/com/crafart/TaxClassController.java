6e0b666c76c7fad10a8e87895792f3fdcfbb371d
Rubiga <rubigakannan@gmail.com>
2015-06-29 19:15:27 +0530

    compilation error fixed

--- crafartweb/src/main/java/com/crafart/TaxClassController.java
+++ crafartweb/src/main/java/com/crafart/TaxClassController.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,19 +32,40 @@ import com.crafart.service.exception.CrafartServiceException;
 public class TaxClassController {
 
 	private static final Logger log = Logger.getLogger(TaxClassController.class);
+	
+		@Autowired
+		private ManageTaxClassService manageTaxClassServiceImpl;
 
-tMapping(value = { " /getTaxClass " }, method = RequestMethod.POST)
-	public @ResponseBody
-alue = { " /getTaxClass " }, method = RequestMethod.POST)
-	public @ResponseBody
-	ModelMap getTaxClassDetail(HttpSession session) {
- @ResponseBody ModelMap getTaxClassDetail(HttpSession session) {
-BOs = new ArrayLis			modelMap.addAttribute("taxClassBOs", taxClassBOs);
-		} catch (CrafartServiceException crafartServiceException) {
-			log.error("Application-error while adding taxdetails", crafartServiceException);
-log.error("Application-error while adding taxdetails", crafartServiceException);
+		
+		@RequestMapping(value = { "/getTaxClass" }, method = RequestMethod.POST)
+		public @ResponseBody
+		ModelMap getTaxClass(HttpSession httpSession) throws CrafartServiceException {
+			ModelMap modelMap = new ModelMap();
+			List<TaxClassBO> taxClassBOs = new ArrayList<>();
+			try {
+				taxClassBOs = manageTaxClassServiceImpl.getTaxClassDetail();
+				modelMap.addAttribute("taxClassBOs", taxClassBOs);
+			} catch (CrafartServiceException crafartServiceException) {
+				log.error("Application-error in retrieving details from DB", crafartServiceException);
+			}
+			return modelMap;
+
 		}
-		return modelMap;
+
+		@RequestMapping(value = { "/addTaxClass" }, method = RequestMethod.POST)
+		public @ResponseBody ModelMap addTaxClass(@RequestBody TaxClassBO TaxClassBO, HttpSession session) {
+			ModelMap modelMap = new ModelMap();
+			try {
+				manageTaxClassServiceImpl.addTaxClassDetail(TaxClassBO);
+				modelMap.addAttribute("result", true);
+			} catch (CrafartServiceException crafartServiceException) {
+				log.error("Application-error while adding to DB", crafartServiceException);
+				modelMap.addAttribute("result", false);
+			}
+			return modelMap;
+
+		}
 
 	}
-}
+
+
