86b1a117c6714bd36e0e6a832d90c6ce3ad03674
Rubiga <rubigakannan@gmail.com>
2015-06-29 19:25:15 +0530

    enhancement - getusergroud details added

--- crafartdataaccess/src/main/java/com/crafart/data/UserGroupDAOImpl.java
+++ crafartdataaccess/src/main/java/com/crafart/data/UserGroupDAOImpl.java
@@ -3,6 +3,9 @@
  */
 package com.crafart.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
@@ -14,7 +17,7 @@ import com.crafart.exception.CrafartDataException;
 import com.crafart.inter.data.UserGroupDAO;
 
 /**
- * @author Deepam
+ * @author
  *
  */
 @Repository("userGroupDAOImpl")
@@ -31,5 +34,19 @@ public class UserGroupDAOImpl extends CommonDAOImpl implements UserGroupDAO {
 		} catch (HibernateException hExp) {
 			throw new CrafartDataException("Error in adding User Group Detail", hExp);
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<UserGroupDO> getUserGroupDetail() throws CrafartDataException {
+		List<UserGroupDO> userGroupDOs = new ArrayList<>();
+		try {
+			Session session = this.getSessionFactory().getCurrentSession();
+			userGroupDOs = session.createQuery("from UserGroupDO").list();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("DB Error while reteriving UserGroup details", hExp);
+		}
+		return userGroupDOs;
 	}
 }
