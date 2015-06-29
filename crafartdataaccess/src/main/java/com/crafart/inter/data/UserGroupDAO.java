4267ad75e04b429fb29cb712cfab8614c95df0c1
Rubiga <rubigakannan@gmail.com>
2015-06-29 19:27:12 +0530

    enhancement - getusergroud details added

--- crafartdataaccess/src/main/java/com/crafart/inter/data/UserGroupDAO.java
+++ crafartdataaccess/src/main/java/com/crafart/inter/data/UserGroupDAO.java
@@ -3,18 +3,22 @@
  */
 package com.crafart.inter.data;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.crafart.dataobjects.UserGroupDO;
 import com.crafart.exception.CrafartDataException;
 
 /**
- * @author Karthi
+ * @author 
  *
  */
 @Repository("UserGroupDAO")
 public interface UserGroupDAO {
 
 	public void addUserGroup(UserGroupDO userGroupDO) throws CrafartDataException;
+
+	public List<UserGroupDO> getUserGroupDetail() throws CrafartDataException;
 
 }
