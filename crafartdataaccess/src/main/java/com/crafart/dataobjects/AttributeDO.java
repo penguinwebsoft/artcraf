aa6e819c5c7e0fcd10637162a4a26c038fc1c289
prabaharanit <prabaharanit@gmail.com>
2015-06-30 20:41:08 +0530

    lazy type fetch added

--- crafartdataaccess/src/main/java/com/crafart/dataobjects/AttributeDO.java
+++ crafartdataaccess/src/main/java/com/crafart/dataobjects/AttributeDO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,7 +36,7 @@ public class AttributeDO implements Cloneable, Serializable {
 	private long attributeId;
 	
 	
-	@ManyToMany(mappedBy = "attributeDOs", cascade=CascadeType.MERGE)
+	@ManyToMany(mappedBy = "attributeDOs", cascade=CascadeType.MERGE, fetch=FetchType.LAZY)
 	private List<CategoryDO> categoryDOs;
 
 	@Column(name = "name")
