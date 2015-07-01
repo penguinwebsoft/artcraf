402b2ec2705d6106680948aba6ccc2c8b5aed459
prabaharanit <prabaharanit@gmail.com>
2015-07-01 13:27:22 +0530

    code refactor- class name corrected to productratingdaoimpl

---
+++ crafartdataaccess/src/main/java/com/crafart/data/ProductRatingDAOImpl.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.crafart.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.dataobjects.ProductRatingDO;
+import com.crafart.exception.CrafartDataException;
+import com.crafart.inter.data.ProductRatingDAO;
+
+/**
+ * @author Karthi
+ * 
+ */
+@Repository("productRatingDAOImpl")
+public class ProductRatingDAOImpl extends CommonDAOImpl implements ProductRatingDAO {
+
+
+	@Override
+	public void addProductRating(ProductRatingDO productRatingDO) throws CrafartDataException {
+		try {
+			Session session = this.getSessionFactory().getCurrentSession();
+			session.save(productRatingDO);
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("DB Error while adding Rating in table", hExp);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<ProductRatingDO> getProductRating(long customerId) throws CrafartDataException {
+		List<ProductRatingDO> productRatingDOs = new ArrayList<>();
+		try {
+			Session session = this.getSessionFactory().getCurrentSession();
+			Query query = session.createQuery("from ProductRatingDO where customer_id = :customer_id");
+			query.setLong("customer_id", customerId);
+			productRatingDOs = (List<ProductRatingDO>) query.list();
+		} catch (EmptyResultDataAccessException hExp) {
+			return null;
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("Error while retriving product ratings details", hExp);
+		}
+		return productRatingDOs;
+	}
+
+}
