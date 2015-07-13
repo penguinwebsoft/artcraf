7945c6acbd2129c284bacec99536cabbded0ba7c
prabaharanit <prabaharanit@gmail.com>
2015-07-13 21:10:39 +0530

    1 .class name corrected 2. add, edit, list funcationality

---
+++ crafartservice/src/main/java/com/crafart/inter/service/ManageCommisionService.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.crafart.inter.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.crafart.service.businessobjects.CommisionBO;
+import com.crafart.service.exception.CrafartServiceException;
+
+/**
+ * @author Karthi
+ *
+ */
+@Service("ManageCommissionService")
+public interface ManageCommisionService {
+
+	public void addCommission(CommisionBO commissionBO) throws CrafartServiceException;
+
+	public Map<Long, CommisionBO> getCommisions()throws CrafartServiceException;
+}
