3689317ce83d252dd016d67db1386a7f7977d696
prabaharanit <prabaharanit@gmail.com>
2015-07-13 21:11:32 +0530

    1 .class name corrected 2. add, edit, list funcationality

---
+++ crafartdataaccess/src/main/java/com/crafart/data/CommisionDAOImpl.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.crafart.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crafart.dataobjects.CommisionDO;
+import com.crafart.exception.CrafartDataException;
+import com.crafart.inter.data.CommissionDAO;
+
+/**
+ * @author Karthi
+ * 
+ */
+@Repository("commissionDAOImpl")
+public class CommisionDAOImpl extends CommonDAOImpl implements CommissionDAO {
+
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void addCommision(CommisionDO commisionDO) throws CrafartDataException {
+		try {
+			Session session = this.getSessionFactory().getCurrentSession();
+			session.save(commisionDO);
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("DB error while adding commission detail", hExp);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<CommisionDO> getCommisions() throws CrafartDataException {
+		List<CommisionDO> commissionDOs = new ArrayList<>();
+		try {
+			Session session = this.getSessionFactory().getCurrentSession();
+			commissionDOs = session.createQuery("from CommissionDO").list();
+		} catch (HibernateException hExp) {
+			throw new CrafartDataException("Error while retriving details from commission table", hExp);
+		}
+		return commissionDOs;
+	}
+
+}
